3492daafd7a8aebc62b9c2c0fedaa2ce0a90b2e5
Adam Reza <adam-reza@hotmail.co.uk>
2024-08-11 12:35:38 +0100

    added availbility to admin

--- playground/admin.py
+++ playground/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Barber, Customer, BarberService, Service, Appointment, AppointmentService
+from .models import Barber, Customer, BarberService, Service, Appointment, AppointmentService, Availability
 
 class BarberServiceInline(admin.TabularInline):
     model = BarberService
@@ -9,6 +9,10 @@ class AppointmentServiceInline(admin.TabularInline):
     model = AppointmentService
     extra = 1  # Number of extra forms
 
+class AvailabilityInline(admin.TabularInline):
+    model = Availability
+    extra = 1  # Number of extra forms
+
 class ServiceAdmin(admin.ModelAdmin):
     inlines = [BarberServiceInline]
 
@@ -16,9 +20,13 @@ class AppointmentAdmin(admin.ModelAdmin):
     list_display = ('user', 'barber', 'date_time', 'end_time')
     inlines = [AppointmentServiceInline]
 
-admin.site.register(Barber)
+class BarberAdmin(admin.ModelAdmin):
+    inlines = [AvailabilityInline]  # Add the Availability inline here
+
+admin.site.register(Barber, BarberAdmin)
 admin.site.register(Customer)
 admin.site.register(BarberService)
 admin.site.register(Service, ServiceAdmin)
 admin.site.register(Appointment, AppointmentAdmin)
 admin.site.register(AppointmentService)
+admin.site.register(Availability)
